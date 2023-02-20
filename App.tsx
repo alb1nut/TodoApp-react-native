c7037a8c41005b9d3b035a66b106d0a4c5e70b3d
Albert Nutifafa Dovlo <albert.dovlo@amalitech.org>
2023-02-20 23:29:12 +0000

    added modeles with topo models

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
-import store from './store';
-import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList';
+// import store from './store';
+// import AddTodo from './components/AddTodo';
+// import TodoList from './components/TodoList';
 
 const styles = StyleSheet.create({
   container: {
