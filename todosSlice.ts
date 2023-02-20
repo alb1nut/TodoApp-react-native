3208752a9ef99cc11b0c336eb8f96a4c1ee493bd
Albert Nutifafa Dovlo <albert.dovlo@amalitech.org>
2023-02-20 12:47:53 +0000

    created redux store and todoSlice an todo typescript interface

---
+++ todosSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Todo } from './types';
+
+interface TodosState {
+  items: Todo[];
+}
+
+const initialState: TodosState = {
+  items: []
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.items.push(action.payload);
+    },
+    toggleTodo: (state, action: PayloadAction<number>) => {
+      const todo = state.items.find(item => item.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    deleteTodo: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    }
+  }
+});
+
+export const { addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
